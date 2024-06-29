59a875f2d3d080d2a5fb86a4d519a3fa8947c663
Ismail <ismailismo78@gmail.com>
2024-06-29 16:53:23 +0100

    implemted toggle method and added indentation

--- script.js
+++ script.js
@@ -3,48 +3,42 @@ let song = document.querySelector(".song");
 let ctrlIocn = document.getElementById("play-pause");
 
 song.onloadedmetadata = () => {
-  progress.max = song.duration;
-  progress.value = song.currentTime;
+    progress.max = song.duration;
+    progress.value = song.currentTime;
 };
 function playPause() {
-  if (ctrlIocn.classList.contains("fa-pause")) {
-    song.pause();
-    ctrlIocn.classList.remove("fa-pause");
-    ctrlIocn.classList.add("fa-play");
-  } else {
-    song.play();
-    ctrlIocn.classList.remove("fa-play");
-    ctrlIocn.classList.add("fa-pause");
-  }
+    ctrlIocn.classList.toggle("fa-pause");
+    ctrlIocn.classList.toggle("fa-play");
+    ctrlIocn.classList.contains("fa-pause") ? song.play() : song.pause();
 }
 if (!song.pause()) {
-  setInterval(() => {
-    progress.value = song.currentTime;
-  }, 500);
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
 }
 progress.onchange = () => {
-  song.currentTime = progress.value;
+    song.currentTime = progress.value;
 };
 
 document.addEventListener("keydown", (e) => {
-  if (e.code === "Space") {
-    playPause();
-  }
+    if (e.code === "Space") {
+        playPause();
+    }
 });
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth < 530) {
-    ctrlIocn.classList.add("fa-3x");
-    ctrlIocn.classList.remove("fa-4x");
-  } else {
-    ctrlIocn.classList.remove("fa-3x");
-    ctrlIocn.classList.add("fa-4x");
-  }
-  if (window.innerWidth < 400) {
-    ctrlIocn.classList.add("fa-2x");
-    ctrlIocn.classList.remove("fa-3x");
-  } else {
-    ctrlIocn.classList.remove("fa-3x");
-    ctrlIocn.classList.add("fa-3x");
-  }
+    if (window.innerWidth < 530) {
+        ctrlIocn.classList.add("fa-3x");
+        ctrlIocn.classList.remove("fa-4x");
+    } else {
+        ctrlIocn.classList.remove("fa-3x");
+        ctrlIocn.classList.add("fa-4x");
+    }
+    if (window.innerWidth < 400) {
+        ctrlIocn.classList.add("fa-2x");
+        ctrlIocn.classList.remove("fa-3x");
+    } else {
+        ctrlIocn.classList.remove("fa-3x");
+        ctrlIocn.classList.add("fa-3x");
+    }
 });
